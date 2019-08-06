101c4daf2450ebcaf8a3739faf295b8156fb8324
Emil Bergstrom <emil.bergstrom@qliro.com>
2019-08-06 21:43:25 +0200

    styling options

--- src/TapRating.js
+++ src/TapRating.js
@@ -5,55 +5,55 @@ import PropTypes from 'prop-types';
 
 import { StyleSheet, Text, View } from 'react-native';
 
-import Star from './components/Star'
+import Star from './components/Star';
 
 export default class TapRating extends Component {
   static defaultProps = {
     defaultRating: 3,
-    reviews: ["Terrible", "Bad", "Okay", "Good", "Great"],
+    reviews: ['Terrible', 'Bad', 'Okay', 'Good', 'Great'],
     count: 5,
     onFinishRating: () => console.log('Rating selected. Attach a function here.'),
     showRating: true
   };
 
   constructor() {
-    super()
+    super();
 
     this.state = {
       position: 5
-    }
+    };
   }
 
   componentDidMount() {
-    const { defaultRating } = this.props
+    const { defaultRating } = this.props;
 
-    this.setState({ position: defaultRating })
+    this.setState({ position: defaultRating });
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.defaultRating !== this.props.defaultRating) {
-      this.setState({ position: nextProps.defaultRating })
+      this.setState({ position: nextProps.defaultRating });
     }
   }
 
   renderStars(rating_array) {
     return _.map(rating_array, (star, index) => {
-      return star
-    })
+      return star;
+    });
   }
 
   starSelectedInPosition(position) {
-    const { onFinishRating } = this.props
+    const { onFinishRating } = this.props;
 
     onFinishRating(position);
 
-    this.setState({ position: position })
+    this.setState({ position: position });
   }
 
   render() {
-    const { position } = this.state
-    const { count, reviews, showRating } = this.props
-    const rating_array = []
+    const { position } = this.state;
+    const { count, reviews, showRating, containerStyle, textStyle } = this.props;
+    const rating_array = [];
 
     _.times(count, index => {
       rating_array.push(
@@ -64,19 +64,13 @@ export default class TapRating extends Component {
           fill={position >= index + 1}
           {...this.props}
         />
-      )
-    })
+      );
+    });
 
     return (
-      <View style={styles.ratingContainer}>
-        { showRating &&
-          <Text style={styles.reviewText}>
-            {reviews[position - 1]}
-          </Text>
-        }
-        <View style={styles.starContainer}>
-          {this.renderStars(rating_array)}
-        </View>
+      <View style={[styles.ratingContainer, containerStyle]}>
+        {showRating && <Text style={[styles.reviewText, textStyle]}>{reviews[position - 1]}</Text>}
+        <View style={styles.starContainer}>{this.renderStars(rating_array)}</View>
       </View>
     );
   }
@@ -87,7 +81,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent',
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   reviewText: {
     fontSize: 25,
@@ -98,6 +92,6 @@ const styles = StyleSheet.create({
   starContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   }
 });
